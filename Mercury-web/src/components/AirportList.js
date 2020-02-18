640bb3bbf429322cfb416501ed84664c71bd4ffe
kowsalyasivasamy <kowsalya@calibraint.com>
2020-02-18 16:59:40 +0530

    #route-fix completed the route changes with component

---
+++ Mercury-web/src/components/AirportList.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import 'antd/dist/antd.less'
+
+const AirportList = () => {
+
+    return (
+        <Fragment>
+            <button>See list of airport</button>
+        </Fragment>
+    )
+}
+
+export default AirportList
---
+++ Mercury-web/src/components/RoutesList.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import 'antd/dist/antd.less'
+
+const RoutesList = () => {
+
+    return (
+        <Fragment>
+            <button>See list of routes</button>
+        </Fragment>
+    )
+}
+
+export default RoutesList
