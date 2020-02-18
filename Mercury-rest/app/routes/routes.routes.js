f74f666f27e56e7c0ed3cffd091a88a834e0d1c6
Ravi Chandhar P B <ravichandhar@Ravis-MacBook-Pro.local>
2020-02-18 20:42:48 +0530

    Route name changed

--- Mercury-rest/app/routes/routes.routes.js
+++ Mercury-rest/app/routes/routes.routes.js
@@ -8,6 +8,6 @@ module.exports = (app) => {
         .post(route.createRoute);
     app.route('/route/:id')
         .delete(route.deleteRoute);
-    app.route('/route/analyse')
+    app.route('/analyse')
         .post(route.analyseRoute);
 }
